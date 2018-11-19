9f224165918f7dbcf2ebb5f21fc401179f373873
chengeyangyang <390518881@qq.com>
2018-11-19 10:59:52 +0800

    添加eureka

--- service-eureka/src/main/java/ch/start/StartEureka.java
+++ service-eureka/src/main/java/ch/start/StartEureka.java
@@ -1,6 +1,7 @@
 package ch.start;
 
 import javafx.application.Application;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
