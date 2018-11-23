9e76d06dd61d140e2955ec445927e9bb1625e37f
chengeyangyang <390518881@qq.com>
2018-11-23 11:41:03 +0800

    将配置中心,注册到服务中心去

--- service-config/src/main/java/ch/configserver/ConfigServiceApplication.java
+++ service-config/src/main/java/ch/configserver/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package ch.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
