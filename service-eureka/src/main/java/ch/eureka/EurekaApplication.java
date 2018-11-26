eee0d7be129da1ed99450bc4ee7e81859a5e26c7
chengeyangyang <390518881@qq.com>
2018-11-26 13:50:49 +0800

    添加数据库设置

--- service-eureka/src/main/java/ch/eureka/EurekaApplication.java
+++ service-eureka/src/main/java/ch/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package ch.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * version 1.0
  */
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EurekaApplication {
 
     public static void main(String[] args) {
